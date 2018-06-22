fd433c5f10a80be2a0ed9b2d00f1688dab48559f
coolsim <coolsimulations@gmail.com>
2018-06-23 08:01:19 +1000

    Fixed Server Proxy

--- src/main/java/net/coolsimulations/MoreThanAPickaxe/Reference.java
+++ src/main/java/net/coolsimulations/MoreThanAPickaxe/Reference.java
@@ -6,6 +6,6 @@ public class Reference {
 	public static final String VERSION = "0.1.0";
 	public static final String ACCEPTED_VERSIONS = "[1.12, 1.12.2]";
 	public static final String CLIENT_PROXY_CLASS = "net.coolsimulations.MoreThanAPickaxe.proxy.ClientProxy";
-	public static final String SERVER_PROXY_CLASS = "net.coolsimulations.MoreThanAPickaxe.ServerProxy";
-	public static final String DEPENDENCIES = "required-after:forge@[14.23.4.2705,);required-after:survivalplus@[0.1.0,);";
+	public static final String SERVER_PROXY_CLASS = "net.coolsimulations.MoreThanAPickaxe.proxy.ServerProxy";
+	public static final String DEPENDENCIES = "required-after:forge@[13.20.0.2288,);required-after:survivalplus@[0.1.0,);";
 }
