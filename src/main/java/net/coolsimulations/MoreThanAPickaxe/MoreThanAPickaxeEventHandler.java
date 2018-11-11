10d5ac7efc1eecc29cc8f0aca82de1c60c4c8373
coolsim <coolsimulations@gmail.com>
2018-11-12 08:45:55 +1000

    Added GC and Steel Support

--- src/main/java/net/coolsimulations/MoreThanAPickaxe/MoreThanAPickaxeEventHandler.java
+++ src/main/java/net/coolsimulations/MoreThanAPickaxe/MoreThanAPickaxeEventHandler.java
@@ -1,10 +1,15 @@
 package net.coolsimulations.MoreThanAPickaxe;
 
 import net.coolsimulations.MoreThanAPickaxe.init.MoreThanAPickaxeItems;
+import net.coolsimulations.SurvivalPlus.api.SPCompatibilityManager;
 import net.minecraft.item.Item;
+import net.minecraft.item.crafting.IRecipe;
+import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.client.event.ModelRegistryEvent;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.oredict.OreDictionary;
+import net.minecraftforge.registries.IForgeRegistryModifiable;
 
 public class MoreThanAPickaxeEventHandler {
 	
@@ -20,6 +25,16 @@ public class MoreThanAPickaxeEventHandler {
         for(Item item : MoreThanAPickaxe.ITEMS) {
         	MoreThanAPickaxeItems.registerRenders();
         }
+    }
+	
+	@SubscribeEvent
+    public void registerRecipes(RegistryEvent.Register<IRecipe> event)
+    {
+		IForgeRegistryModifiable modRegistry = (IForgeRegistryModifiable) event.getRegistry();
+		
+		if(SPCompatibilityManager.isGCLoaded()) {
+			modRegistry.remove(new ResourceLocation(Reference.MOD_ID + ":" + "steel_adze"));	
+		}
     }
 
 }
--- src/main/java/net/coolsimulations/MoreThanAPickaxe/init/MoreThanAPickaxeItems.java
+++ src/main/java/net/coolsimulations/MoreThanAPickaxe/init/MoreThanAPickaxeItems.java
@@ -1,7 +1,11 @@
 package net.coolsimulations.MoreThanAPickaxe.init;
 
+import micdoodle8.mods.galacticraft.core.GCItems;
+import micdoodle8.mods.galacticraft.planets.asteroids.items.AsteroidsItems;
+import micdoodle8.mods.galacticraft.planets.mars.items.MarsItems;
 import net.coolsimulations.MoreThanAPickaxe.MoreThanAPickaxe;
 import net.coolsimulations.MoreThanAPickaxe.item.ItemAdze;
+import net.coolsimulations.SurvivalPlus.api.SPCompatibilityManager;
 import net.coolsimulations.SurvivalPlus.api.SPItems;
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.item.Item;
@@ -9,6 +13,9 @@ import net.minecraft.item.Item.ToolMaterial;
 import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 import net.minecraftforge.registries.IForgeRegistry;
+import net.minecraftforge.common.util.EnumHelper;
+import net.minecraftforge.fml.common.registry.GameRegistry;
+import net.minecraftforge.oredict.OreDictionary;
 
 public class MoreThanAPickaxeItems {
 	
@@ -20,6 +27,12 @@ public class MoreThanAPickaxeItems {
 	public static Item bronze_adze;
 	public static Item titanium_adze;
 	
+	public static Item steel_adze;
+	public static Item desh_adze;
+	public static Item titanium_adze_gc;
+	
+    public static ToolMaterial steelToolMaterial = EnumHelper.addToolMaterial("steel_adze", 2, 500, 7, 2.5F, 9);
+	
 	public static void init() {
 		
 		wooden_adze = new ItemAdze(ToolMaterial.WOOD, 6.0F, -2.4F).setUnlocalizedName("wooden_adze").setRegistryName("wooden_adze").setCreativeTab(MoreThanAPickaxe.tabMoreThanAPickaxe);
@@ -29,6 +42,25 @@ public class MoreThanAPickaxeItems {
 		diamond_adze = new ItemAdze(ToolMaterial.DIAMOND, 5.0F, -2.4F).setUnlocalizedName("diamond_adze").setRegistryName("diamond_adze").setCreativeTab(MoreThanAPickaxe.tabMoreThanAPickaxe);
 		bronze_adze = new ItemAdze(SPItems.bronzeToolMaterial, 5.5F, -2.4F).setUnlocalizedName("bronze_adze").setRegistryName("bronze_adze").setCreativeTab(MoreThanAPickaxe.tabMoreThanAPickaxe);
 		titanium_adze = new ItemAdze(SPItems.titaniumToolMaterial, 5.5F, -2.4F).setUnlocalizedName("titanium_adze").setRegistryName("titanium_adze").setCreativeTab(MoreThanAPickaxe.tabMoreThanAPickaxe);
+		
+		if(OreDictionary.getOres("ingotSteel").size() > 0) {
+			
+			if(!SPCompatibilityManager.isGCLoaded()) {
+				
+				steel_adze = new ItemAdze(steelToolMaterial, 5.5F, -2.4F).setUnlocalizedName("steel_adze").setRegistryName("steel_adze").setCreativeTab(MoreThanAPickaxe.tabMoreThanAPickaxe);
+			}
+		}
+		
+		if(SPCompatibilityManager.isGCLoaded()) {
+			
+			steel_adze = new ItemAdze(GCItems.TOOL_STEEL, 4.0F, -2.4F).setUnlocalizedName("steel_adze").setRegistryName("steel_adze").setCreativeTab(MoreThanAPickaxe.tabMoreThanAPickaxe);
+			
+			if(SPCompatibilityManager.isGCPLoaded()) {
+				
+				desh_adze = new ItemAdze(MarsItems.TOOLDESH, 3.5F, -2.4F).setUnlocalizedName("desh_adze").setRegistryName("desh_adze").setCreativeTab(MoreThanAPickaxe.tabMoreThanAPickaxe);
+				titanium_adze_gc = new ItemAdze(AsteroidsItems.TOOL_TITANIUM, 3.0F, -2.4F).setUnlocalizedName("titanium_adze_gc").setRegistryName("titanium_adze_gc").setCreativeTab(MoreThanAPickaxe.tabMoreThanAPickaxe);
+			}
+		}
 	}
 	
 	public static void register()
@@ -41,6 +73,25 @@ public class MoreThanAPickaxeItems {
 		registerItem(diamond_adze);
 		registerItem(bronze_adze);
 		registerItem(titanium_adze);
+		
+		if(OreDictionary.getOres("ingotSteel").size() > 0) {
+			
+			if(!SPCompatibilityManager.isGCLoaded()) {
+				
+				registerItem(steel_adze);
+			}
+		}
+		
+		if(SPCompatibilityManager.isGCLoaded()) {
+			
+			registerItem(steel_adze);
+			
+			if(SPCompatibilityManager.isGCPLoaded()) {
+				
+				registerItem(desh_adze);
+				registerItem(titanium_adze_gc);
+			}
+		}
 	}
 	
 	public static void registerRenders()
@@ -52,6 +103,25 @@ public class MoreThanAPickaxeItems {
 		registerRender(diamond_adze);
 		registerRender(bronze_adze);
 		registerRender(titanium_adze);
+		
+		if(OreDictionary.getOres("ingotSteel").size() > 0) {
+			
+			if(!SPCompatibilityManager.isGCLoaded()) {
+				
+				registerRender(steel_adze);
+			}
+		}
+		
+		if(SPCompatibilityManager.isGCLoaded()) {
+			
+			registerRender(steel_adze);
+			
+			if(SPCompatibilityManager.isGCPLoaded()) {
+				
+				registerRender(desh_adze);
+				registerRender(titanium_adze_gc);
+			}
+		}
 	}
 	
 	public static void registerItem(Item item) {
