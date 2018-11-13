7db58407a0456112533309d2dee9e080440c445c
coolsim <coolsimulations@gmail.com>
2018-11-14 09:32:38 +1000

    Added Event Handler in Main Class

--- src/main/java/net/coolsimulations/MoreThanAPickaxe/MoreThanAPickaxe.java
+++ src/main/java/net/coolsimulations/MoreThanAPickaxe/MoreThanAPickaxe.java
@@ -4,6 +4,7 @@ import net.coolsimulations.MoreThanAPickaxe.init.MoreThanAPickaxeItems;
 import net.coolsimulations.MoreThanAPickaxe.proxy.CommonProxy;
 import net.coolsimulations.MoreThanAPickaxe.recipes.MoreThanAPickaxeRecipes;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.SidedProxy;
@@ -26,6 +27,7 @@ public class MoreThanAPickaxe {
 	public void preInit(FMLPreInitializationEvent event)
 	{
 		System.out.println("Pre Init");
+		MinecraftForge.EVENT_BUS.register(new MoreThanAPickaxeEventHandler());
 		MoreThanAPickaxeUpdateHandler.init();
 		MoreThanAPickaxeItems.init();
 		MoreThanAPickaxeItems.register();
