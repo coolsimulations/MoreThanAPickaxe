bce98f468170b1cde11b4a38842d59316f6fdd05
coolsim <coolsimulations@gmail.com>
2019-10-26 14:55:15 +1000

    Fixed Dependencies

--- src/main/java/net/coolsimulations/MoreThanAPickaxe/Reference.java
+++ src/main/java/net/coolsimulations/MoreThanAPickaxe/Reference.java
@@ -7,5 +7,5 @@ public class Reference {
 	public static final String ACCEPTED_VERSIONS = "[1.12, 1.12.2]";
 	public static final String CLIENT_PROXY_CLASS = "net.coolsimulations.MoreThanAPickaxe.proxy.ClientProxy";
 	public static final String SERVER_PROXY_CLASS = "net.coolsimulations.MoreThanAPickaxe.proxy.ServerProxy";
-	public static final String DEPENDENCIES = "required-after:forge@[13.20.0.2288,);required-after:survivalplus@[0.1.0,);";
+	public static final String DEPENDENCIES = "required-after:forge@[13.20.0.2288,);required-after:survivalplus@[0.1.1,);";
 }
