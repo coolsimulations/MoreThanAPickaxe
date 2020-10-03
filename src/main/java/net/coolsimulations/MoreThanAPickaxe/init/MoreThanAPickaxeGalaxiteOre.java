58e335765571ed99ab4ef3bb054b2bc4eac7d34b
coolsim <coolsimulations@gmail.com>
2020-10-03 14:36:29 +1000

    Added Vulcanite/Galaxite Special Abilities

--- src/main/java/net/coolsimulations/MoreThanAPickaxe/init/MoreThanAPickaxeGalaxiteOre.java
+++ src/main/java/net/coolsimulations/MoreThanAPickaxe/init/MoreThanAPickaxeGalaxiteOre.java
@@ -1,10 +1,17 @@
 package net.coolsimulations.MoreThanAPickaxe.init;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import net.coolsimulations.MoreThanAPickaxe.item.ItemAdze;
 import net.coolsimulations.SurvivalPlus.api.SPTabs;
 import net.insane96mcp.galaxite.init.ModItems;
 import net.insane96mcp.galaxite.item.material.ModMaterial;
+import net.insane96mcp.galaxite.events.PlayerBreakSpeed;
+import net.insane96mcp.galaxite.events.LivingHurt;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.fml.common.ObfuscationReflectionHelper;
 
 public class MoreThanAPickaxeGalaxiteOre {
 	
@@ -13,6 +20,8 @@ public class MoreThanAPickaxeGalaxiteOre {
 	public static void init() {
 		
 		galaxite_adze = new ItemAdze(ModMaterial.Tool, 6.5F, -2.4F).setUnlocalizedName("galaxite_adze").setRegistryName("galaxite_adze").setCreativeTab(SPTabs.tabTools);
+		registerLivingList();
+		registerBreakList();
 	}
 	
 	public static void register() {
@@ -25,6 +34,34 @@ public class MoreThanAPickaxeGalaxiteOre {
 	
 	public static void registerToolMaterial() {
 		MoreThanAPickaxeToolMaterials.setMaterial(MoreThanAPickaxeGalaxiteOre.galaxite_adze, ModItems.galaxite);
+	}
+	
+	public static void registerLivingList() {
+
+		ItemStack[] galaxiteTools = ObfuscationReflectionHelper.getPrivateValue(LivingHurt.class, null, "galaxiteTools");
+		List<ItemStack> newList = new ArrayList<ItemStack>();
+
+		for(ItemStack stack : galaxiteTools)
+			newList.add(stack);
+
+		if(!newList.contains(new ItemStack(galaxite_adze)))
+			newList.add(new ItemStack(galaxite_adze));
+
+		ObfuscationReflectionHelper.setPrivateValue(LivingHurt.class, null, newList.toArray(new ItemStack[0]), "galaxiteTools");
+	}
+	
+	public static void registerBreakList() {
+
+		ItemStack[] validTools = ObfuscationReflectionHelper.getPrivateValue(PlayerBreakSpeed.class, null, "validEfficencyBoost");
+		List<ItemStack> newList = new ArrayList<ItemStack>();
+
+		for(ItemStack stack : validTools)
+			newList.add(stack);
+
+		if(!newList.contains(new ItemStack(galaxite_adze)))
+			newList.add(new ItemStack(galaxite_adze));
+
+		ObfuscationReflectionHelper.setPrivateValue(PlayerBreakSpeed.class, null, newList.toArray(new ItemStack[0]), "validEfficencyBoost");
 	}
 
 }
--- src/main/java/net/coolsimulations/MoreThanAPickaxe/init/MoreThanAPickaxeVulcanite.java
+++ src/main/java/net/coolsimulations/MoreThanAPickaxe/init/MoreThanAPickaxeVulcanite.java
@@ -1,30 +1,83 @@
 package net.coolsimulations.MoreThanAPickaxe.init;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import net.coolsimulations.MoreThanAPickaxe.item.ItemAdze;
 import net.coolsimulations.SurvivalPlus.api.SPTabs;
+import net.insane96mcp.vulcanite.events.HarvestDrops;
+import net.insane96mcp.vulcanite.events.LivingHurt;
+import net.insane96mcp.vulcanite.events.PlayerBreakSpeed;
 import net.insane96mcp.vulcanite.init.ModItems;
 import net.insane96mcp.vulcanite.item.material.ModMaterial;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.fml.common.ObfuscationReflectionHelper;
 
 public class MoreThanAPickaxeVulcanite {
-	
+
 	public static Item vulcanite_adze;
-	
+
 	public static void init() {
-		
+
 		vulcanite_adze = new ItemAdze(ModMaterial.tool, 4.5F, -2.4F).setUnlocalizedName("vulcanite_adze").setRegistryName("vulcanite_adze").setCreativeTab(SPTabs.tabTools);
+		registerHarvestList();
+		registerLivingList();
+		registerBreakList();
 	}
-	
+
 	public static void register() {
 		MoreThanAPickaxeItems.registerItem(vulcanite_adze);
 	}
-	
+
 	public static void registerRenders() {
 		MoreThanAPickaxeItems.registerRender(vulcanite_adze);
 	}
-	
+
 	public static void registerToolMaterial() {
 		MoreThanAPickaxeToolMaterials.setMaterial(MoreThanAPickaxeVulcanite.vulcanite_adze, ModItems.vulcaniteIngotItem);
+	}
+
+	public static void registerHarvestList() {
+
+		ItemStack[] validTools = ObfuscationReflectionHelper.getPrivateValue(HarvestDrops.class, null, "validTools");
+		List<ItemStack> newList = new ArrayList<ItemStack>();
+
+		for(ItemStack stack : validTools)
+			newList.add(stack);
+
+		if(!newList.contains(new ItemStack(vulcanite_adze)))
+			newList.add(new ItemStack(vulcanite_adze));
+
+		ObfuscationReflectionHelper.setPrivateValue(HarvestDrops.class, null, newList.toArray(new ItemStack[0]), "validTools");
+	}
+
+	public static void registerLivingList() {
+
+		ItemStack[] vulcaniteWeapons = ObfuscationReflectionHelper.getPrivateValue(LivingHurt.class, null, "vulcaniteWeapons");
+		List<ItemStack> newList = new ArrayList<ItemStack>();
+
+		for(ItemStack stack : vulcaniteWeapons)
+			newList.add(stack);
+
+		if(!newList.contains(new ItemStack(vulcanite_adze)))
+			newList.add(new ItemStack(vulcanite_adze));
+
+		ObfuscationReflectionHelper.setPrivateValue(LivingHurt.class, null, newList.toArray(new ItemStack[0]), "vulcaniteWeapons");
+	}
+
+	public static void registerBreakList() {
+
+		ItemStack[] validTools = ObfuscationReflectionHelper.getPrivateValue(PlayerBreakSpeed.class, null, "validEfficencyBoost");
+		List<ItemStack> newList = new ArrayList<ItemStack>();
+
+		for(ItemStack stack : validTools)
+			newList.add(stack);
+
+		if(!newList.contains(new ItemStack(vulcanite_adze)))
+			newList.add(new ItemStack(vulcanite_adze));
+
+		ObfuscationReflectionHelper.setPrivateValue(PlayerBreakSpeed.class, null, newList.toArray(new ItemStack[0]), "validEfficencyBoost");
 	}
 
 }
