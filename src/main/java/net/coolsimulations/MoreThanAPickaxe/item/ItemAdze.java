24b827bcc397465df3d943397307ec50e27e8e0e
coolsim <coolsimulations@gmail.com>
2021-07-09 10:20:38 +1000

    Fixed ActionResultType

--- src/main/java/net/coolsimulations/MoreThanAPickaxe/item/ItemAdze.java
+++ src/main/java/net/coolsimulations/MoreThanAPickaxe/item/ItemAdze.java
@@ -148,7 +148,7 @@ public class ItemAdze extends ToolItem{
 			if (hook != 0) return hook > 0 ? ActionResultType.SUCCESS : ActionResultType.FAIL;
 			if(context.getFace() != Direction.DOWN) {
 				if (world.isAirBlock(blockpos.up())) {
-					setBlockToFarmland(context, blockpos, world);
+					return setBlockToFarmland(context, blockpos, world);
 				}
 
 				if(block instanceof BushBlock) {
@@ -178,12 +178,14 @@ public class ItemAdze extends ToolItem{
 					}
 				}
 			}
+			
+			return ActionResultType.PASS;
 		} else {
 			if(context.getFace() != Direction.DOWN) {
 				PlayerEntity playerentity = context.getPlayer();
 
 				if (world.getBlockState(blockpos.up()).isAir()) {
-					setBlockToPath(context, blockpos, world);
+					return setBlockToPath(context, blockpos, world);
 				}
 
 				if(block instanceof BushBlock) {
@@ -214,9 +216,8 @@ public class ItemAdze extends ToolItem{
 				}
 
 			}
+			return ActionResultType.PASS;
 		}
-
-		return ActionResultType.PASS;
 	}
 
 	protected ActionResultType setBlockToFarmland(ItemUseContext context, BlockPos blockpos, World world) {
