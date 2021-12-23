83d9b7a6f2ed7e131b7ce4aadbf3c8d8d93d0abe
coolsimulations <coolsimulations@gmail.com>
2021-12-23 16:04:44 +1000

    Fixed and Updated JEI Support

--- src/main/java/net/coolsimulations/MoreThanAPickaxe/init/MoreThanAPickaxeCarbonado.java
+++ src/main/java/net/coolsimulations/MoreThanAPickaxe/init/MoreThanAPickaxeCarbonado.java
@@ -1,10 +1,19 @@
 package net.coolsimulations.MoreThanAPickaxe.init;
 
+import java.util.Collections;
+
+import javax.annotation.Nonnull;
+
+import mezz.jei.api.IModRegistry;
+import mezz.jei.api.recipe.VanillaRecipeCategoryUid;
+import mezz.jei.plugins.vanilla.anvil.AnvilRecipeWrapper;
 import net.coolsimulations.MoreThanAPickaxe.item.ItemAdze;
 import net.coolsimulations.SurvivalPlus.api.SPTabs;
 import net.insane96mcp.carbonado.init.ModItems;
 import net.insane96mcp.carbonado.item.material.ModMaterial;
+import net.insane96mcp.carbonado.lib.Properties;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 
 public class MoreThanAPickaxeCarbonado {
 	
@@ -25,6 +34,14 @@ public class MoreThanAPickaxeCarbonado {
 	
 	public static void registerToolMaterial() {
 		MoreThanAPickaxeToolMaterials.setMaterial(MoreThanAPickaxeCarbonado.carbonado_adze, ModItems.carbonadoItem);
+	}
+	
+	public static void initJEI(@Nonnull IModRegistry registry) {
+		
+		if(Properties.config.tools.enableAnvilCrafting) {
+			AnvilRecipeWrapper anvilRecipe = new AnvilRecipeWrapper(Collections.singletonList(new ItemStack(MoreThanAPickaxeItems.diamond_adze)), Collections.singletonList(new ItemStack(ModItems.carbonadoItem, 4)), Collections.singletonList(new ItemStack(MoreThanAPickaxeCarbonado.carbonado_adze)));
+			registry.addRecipes(Collections.singletonList(anvilRecipe), VanillaRecipeCategoryUid.ANVIL);
+		}
 	}
 
 }
--- src/main/java/net/coolsimulations/MoreThanAPickaxe/init/MoreThanAPickaxeJEI.java
+++ src/main/java/net/coolsimulations/MoreThanAPickaxe/init/MoreThanAPickaxeJEI.java
@@ -1,26 +1,21 @@
 package net.coolsimulations.MoreThanAPickaxe.init;
 
-import java.util.Collections;
-
 import javax.annotation.Nonnull;
 
-import mezz.jei.api.BlankModPlugin;
+import mezz.jei.api.IModPlugin;
 import mezz.jei.api.IModRegistry;
 import mezz.jei.api.JEIPlugin;
 import net.coolsimulations.SurvivalPlus.api.SPCompatibilityManager;
-import net.insane96mcp.carbonado.init.ModItems;
-import net.insane96mcp.carbonado.lib.Properties;
 import net.minecraft.item.ItemStack;
 
 @JEIPlugin
-public class MoreThanAPickaxeJEI extends BlankModPlugin{
+public class MoreThanAPickaxeJEI implements IModPlugin{
 	
 	@Override
 	public void register(@Nonnull IModRegistry registry) {
 
 		if(SPCompatibilityManager.isCarbonadoLoaded())
-			if(Properties.config.tools.enableAnvilCrafting)
-			registry.addAnvilRecipe(new ItemStack(MoreThanAPickaxeItems.diamond_adze), Collections.singletonList(new ItemStack(ModItems.carbonadoItem, 4)), Collections.singletonList(new ItemStack(MoreThanAPickaxeCarbonado.carbonado_adze)));
+			MoreThanAPickaxeCarbonado.initJEI(registry);
 		
 		if(SPCompatibilityManager.isNoTreePunchingLoaded())
 			registry.getJeiHelpers().getIngredientBlacklist().addIngredientToBlacklist(new ItemStack(MoreThanAPickaxeItems.wooden_adze));
