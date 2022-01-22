7c9c75195717cf0b6d9d16886ffb7227307ede82
coolsimulations <coolsimulations@gmail.com>
2022-01-22 15:10:44 +1000

    Version 0.1.5c

--- src/main/java/net/coolsimulations/MoreThanAPickaxe/Reference.java
+++ src/main/java/net/coolsimulations/MoreThanAPickaxe/Reference.java
@@ -3,7 +3,7 @@ package net.coolsimulations.MoreThanAPickaxe;
 public class Reference {
 	public static final String MOD_ID = "morethanapickaxe";
 	public static final String MOD_NAME = "More Than A Pickaxe";
-	public static final String VERSION = "0.1.5b";
+	public static final String VERSION = "0.1.5c";
 	public static final String ACCEPTED_VERSIONS = "[1.14, 1.14.4]";
 	public static final String CLIENT_PROXY_CLASS = "net.coolsimulations.MoreThanAPickaxe.proxy.ClientProxy";
 	public static final String SERVER_PROXY_CLASS = "net.coolsimulations.MoreThanAPickaxe.proxy.ServerProxy";
